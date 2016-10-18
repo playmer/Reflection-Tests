ba4937c16ebcb407c501a1c4ffafab6f612c1e8d
Joshua T. Fisher <playmer@gmail.com>
2016-10-18 00:48:51 -0700

    Nontemplated Structs and Classes working on MSVC and Clang

--- ReflectionTest/Source.cpp
+++ ReflectionTest/Source.cpp
@@ -30,26 +30,66 @@ struct UrKidStruct {};
 
 class UrKidClass {};
 
-template <size_t Size>
+template <size_t ConstSize>
 struct ConstexprToken
 {
   constexpr ConstexprToken(const char *aBegin)
     : mData{'0'}
   {
-    for (size_t i = 0; i < Size; i++)
+    for (size_t i = 0; i < ConstSize; i++)
     {
       mData[i] = aBegin[i];
     }
 
-    mData[Size] = '\0';
+    mData[ConstSize] = '\0';
   }
 
+  constexpr size_t Size() const { return ConstSize; };
+  constexpr size_t size() const { return ConstSize; };
   constexpr const char* Data() const { return mData; };
   constexpr const char* data() const { return mData; };
 
 private:
-  char mData[Size + 1];
+  char mData[ConstSize + 1];
 };
+
+
+constexpr size_t GetLastInstanceOfCharacter(const char *aTypeNameString, size_t aSize, char aCharacter)
+{
+  size_t toReturn = aSize + 1;
+
+  while (aSize != 0)
+  {
+    if (aTypeNameString[aSize] == aCharacter)
+    {
+      toReturn = aSize;
+      break;
+    }
+    --aSize;
+  }
+
+  return toReturn;
+}
+
+
+constexpr size_t GetFirstInstanceOfCharacter(const char *aTypeNameString, size_t aSize, char aCharacter)
+{
+  size_t toReturn = aSize + 1;
+
+  size_t i = 0;
+
+  while (i != aSize)
+  {
+    if (aTypeNameString[aSize] == aCharacter)
+    {
+      toReturn = aSize;
+      break;
+    }
+    ++i;
+  }
+
+  return toReturn;
+}
 
 constexpr size_t GetTypeStart(const char *aTypeNameString)
 {
@@ -100,29 +140,33 @@ constexpr auto GetTypeName()
 {
   constexpr const char* typeName = CONSTEXPR_FUNCTION_SIGNATURE;
 
-  const char *fullTypeName = typeName;
+  constexpr size_t totalLength = StringLength(typeName);
+  constexpr size_t lastSpace = GetLastInstanceOfCharacter(typeName, StringLength(typeName), ' ');
+  constexpr size_t typeNameLength = totalLength - lastSpace - GetTypeEnd() - 1;
 
-  constexpr size_t required_length = StringLength(typeName);
-  ConstexprToken<required_length> test{ typeName };
+  ConstexprToken<typeNameLength> token{ typeName + lastSpace + 1 };
 
-  return fullTypeName;
+  return token;
 }
 
-void Func1(UrKidStruct aStruct) {}
-void Func2(UrKidClass aStruct) {}
+namespace Function
+{
+  void Func1(UrKidStruct aStruct) {}
+  void Func2(UrKidClass aStruct) {}
+}
 
 int main()
 {
-  constexpr const char *doesThisWork = GetTypeName<Test::Test2::UrMum>();
-  std::cout << doesThisWork << std::endl;
-  std::cout << GetTypeName<Test::UrSun>() << std::endl;
-  std::cout << GetTypeName<UrKidClass>() << std::endl;
+  constexpr auto doesThisWork = GetTypeName<Test::Test2::UrMum>();
+  std::cout << doesThisWork.data() << std::endl;
+  std::cout << GetTypeName<Test::UrSun>().data() << std::endl;
+  std::cout << GetTypeName<UrKidClass>().data() << std::endl;
 
-  constexpr size_t required_length = StringLength(GetTypeName<UrKidClass>());
+  constexpr size_t required_length = GetTypeName<UrKidClass>().Size();
   std::cout << required_length << std::endl;
-  std::cout << GetTypeName<UrKidStruct>() << std::endl;
+  std::cout << GetTypeName<UrKidStruct>().data() << std::endl;
 
-  constexpr auto func1 = GetFunctionSignature<decltype(Func1)*, Func1>();
+  constexpr auto func1 = GetFunctionSignature<decltype(Function::Func1)*, Function::Func1>();
   //constexpr auto func2 = GetFunctionSignature<decltype(Func2)*, Func2>();
   std::cout << func1.data() << std::endl;
   //printf("%s\n", func2.mData);

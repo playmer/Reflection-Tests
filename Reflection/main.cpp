7c1bb5c12b0b8d69897ee9ce7cfd5454e2efd022
Joshua T. Fisher <playmer@gmail.com>
2018-04-27 10:48:34 -0700

    Expand repro

--- Reflection/main.cpp
+++ Reflection/main.cpp
@@ -24,10 +24,21 @@ namespace YTE
     Unknown
   };
 
+
+  static XboxController* STATIC_GetXboxController(ControllerId aId)
+  {
+    return nullptr;
+  }
+
   class GamepadSystem
   {
   public:
     YTEDeclareType(GamepadSystem);
+    XboxController* GetXboxControllerConst(ControllerId aId) const
+    {
+      return nullptr;
+    }
+
     XboxController* GetXboxController(ControllerId aId)
     {
       return nullptr;
@@ -37,10 +48,6 @@ namespace YTE
   YTEDefineType(GamepadSystem)
   {
     YTERegisterType(GamepadSystem);
-    //auto size = CountFunctionArguments<decltype(&GamepadSystem::GetXboxController)>::template Size();
-
-    //YTEBindFunction(&GamepadSystem::GetXboxController, YTENoOverload, "GetXboxController", YTEParameterNames("aController")).Description()
-    //  = "Gets you the given controller. Warning: Controller may not be plugged in/active.";
   }
 }
 
@@ -49,16 +56,12 @@ YTEDeclareExternalType(YTE::ControllerId);
 YTEDefineExternalType(YTE::ControllerId)
 {
   YTERegisterType(YTE::ControllerId);
-
-  //  YTEBindEnumValue(ControllerId::Xbox_P1, "Xbox_P1");
-  //  YTEBindEnumValue(ControllerId::Xbox_P2, "Xbox_P2");
-  //  YTEBindEnumValue(ControllerId::Xbox_P3, "Xbox_P3");
-  //  YTEBindEnumValue(ControllerId::Xbox_P4, "Xbox_P4");
-  //  YTEBindEnumValue(ControllerId::Xbox_Controllers_Number, "Xbox_Controllers_Number");
-  //  YTEBindEnumValue(ControllerId::Unknown, "Unknown");
 }
 
 int main()
 {
-  auto caller = YTE::Binding<decltype(&YTE::GamepadSystem::GetXboxController)>::template Caller<&YTE::GamepadSystem::GetXboxController>;
+  // These will all ICE.
+  auto caller1 = YTE::Binding<decltype(&YTE::STATIC_GetXboxController)>::template Caller<&YTE::STATIC_GetXboxController>;
+  auto caller2 = YTE::Binding<decltype(&YTE::GamepadSystem::GetXboxControllerConst)>::template Caller<&YTE::GamepadSystem::GetXboxControllerConst>;
+  auto caller3 = YTE::Binding<decltype(&YTE::GamepadSystem::GetXboxController)>::template Caller<&YTE::GamepadSystem::GetXboxController>;
 }
